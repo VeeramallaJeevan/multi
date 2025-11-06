d363a60151e9785469075e5757b5fc56365635ff
jeevan <v.jeevan.2005@gmail.com>
2025-11-06 12:05:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
   public static void main(String args[]){
-    int a=10;
-  int b=20;
-int c = a+b;
-System.out.print("sum:",c);
+    int a= 10;
+    int b= 20;
+    int c = a+b;
+    System.out.print(c);
 }
 }
